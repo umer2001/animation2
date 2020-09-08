803ba2eac5abd4efff2565cdc2b29d0293583c7b
umer2001 <umer2001.uf@gmail.com>
2020-09-08 15:58:35 +0500

    add: website animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Documents from "./components/Documents";
+import Website from "./components/Website";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Documents />
+        <Website />
       </header>
     </div>
   );
